74a58376d4d580e2d85d79dea3b1e06f3a7d4d2a
Gebruiker <harold65@users.noreply.github.com>
2013-07-25 12:55:55 +0200

    fixed exosite response logging

--- Exosite.ino
+++ Exosite.ino
@@ -9,9 +9,10 @@ void SendToExosite()
     if (exoSiteClient.connect("m2.exosite.com",80)) 
     {
         long v[9];
-        
+        // fill the v-array with values defined in the exosite.h file
         #include "exosite.h"
         
+        // compose a string with data to send
         String str = "";
         for(int i = 0; i < 9; i++)
         {
@@ -20,17 +21,25 @@ void SendToExosite()
             str += '=';
             str += v[i];
         }        
+        // send the header
         exoSiteClient << F("POST /api:v1/stack/alias HTTP/1.1") << endl;
         exoSiteClient << F("Host: m2.exosite.com") << endl;
         exoSiteClient << F("X-Exosite-CIK: " EXOSITE_KEY) << endl;
         exoSiteClient << F("Content-Type: application/x-www-form-urlencoded; charset=utf-8") << endl;
+        // send the length of the data
         exoSiteClient << F("Content-Length: ") << str.length() << endl << endl;
+        // send the data itself
         exoSiteClient << str << endl;
+        // skip the first part of the response ('HTTP/1.1 ')
+        exositeClient.find(' ');
+        // the characters after the space form the response number, read it
         exResponse = exoSiteClient.parseInt();
+        // close the connection
         exoSiteClient.stop();
     }
     else
     {
+        // unable to connect with exosite
         exResponse = 0;
     }
 }
